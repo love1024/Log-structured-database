3b56907af109004fa0639be9574931b341fb6f86
LP <lsingh@questrade.com>
2024-04-02 22:40:27 -0400

    renamed files

--- db.js
+++ db.js
@@ -1,70 +1,15 @@
-const fs = require("fs");
-const fsR = require("fs-reverse");
-const nthline = require("nthline");
-const readline = require("readline");
-const DB_FILE = "file.txt";
+const db = require("./storage");
 
-const map = new Map();
-let lineNumbers = 0;
+// Hydrate map once the server is started
+// To set all line numbers and keys
+// Not needed every time like here
+db.hydrateMap(() => {
+  const key = process.argv.length > 2 ? process.argv[2] : null;
+  const value = process.argv.length > 3 ? process.argv[3] : null;
 
-// Read operation
-function read(key) {
-  if (map.has(key)) {
-    const ln = map.get(key);
-    nthline(ln, DB_FILE).then((line) => {
-      console.log(line.split(",")[1]);
-    });
-  } else {
-    console.log("Record not found.");
+  if (key && value) {
+    db.write(key, value);
+  } else if (key) {
+    db.read(key);
   }
-}
-
-// Write operation
-function write(key, value) {
-  fs.appendFile(DB_FILE, key + "," + value + "\r\n", function (err) {
-    if (err) throw err;
-    map.set(key, lineNumbers);
-    lineNumbers++;
-    console.log("Write successful");
-  });
-}
-
-async function hydrateMap(cb) {
-  const fileStream = fs.createReadStream(DB_FILE);
-  const rl = readline.createInterface({
-    input: fileStream,
-  });
-
-  for await (const line of rl) {
-    // Check if the key is equal to the provided key
-    const key = line.split(",")[0];
-    map.set(key, lineNumbers);
-    lineNumbers++;
-  }
-
-  cb();
-}
-
-module.exports = {
-  read,
-  write,
-  hydrateMap,
-};
-
-// Function without using indexes
-// function read(key) {
-//   const readStream = fsR(DB_FILE, {});
-
-//   readStream.on("data", (line) => {
-//     // have this check to make sure empty lines are not parsed
-//     // and stream is not destroyed yet
-//     if (line && !readStream.destroyed) {
-//       // Check if the key is equal to the provided key
-//       const k = line ? line.split(",")[0] : null;
-//       if (k === key) {
-//         console.log(line.split(",")[1]);
-//         readStream.destroy();
-//       }
-//     }
-//   });
-// }
+});
